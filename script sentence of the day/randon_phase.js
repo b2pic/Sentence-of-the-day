b17a3b9062d2a5c3fccd041c28783f7197e725e4
b2pic <mpamimpamimael001@gmail.com>
2022-12-07 07:53:46 +0100

    modified file html css  and  folder script sentence

---
+++ script sentence of the day/randon_phase.js
@@ -0,0 +1,76 @@
+const dataImgSentene = {
+  dataSentene: ["Soyez vous-même tous les autres sont déjà pris » - Oscar Wilde", "J’ai raté 9 000 tirs dans ma carrière. J’ai perdu presque 300 matchs. 26 fois, on m’a fait confiance pour prendre le tir de la victoire et j’ai raté. J’ai échoué encore et encore et encore dans ma vie. Et c’est pourquoi je réussis. » – Michael Jordan",
+   
+  "« Dans 20 ans, vous serez plus déçu par les choses que vous n’avez pas faites que par celles que vous avez faites. Alors, sortez des sentiers battus. Mettez les voiles. Explorez. Rêvez. Découvrez. » – Mark Twain",
+ 
+  "« Faites ce que vous pouvez, avec ce que vous avez, là où vous êtes. » – Theodore Roosevelt",
+ 
+  "« Toute personne qui n’a jamais commis d’erreurs n’a jamais tenté d’innover. » – Albert Einstein",
+ 
+  "« Je connais un moyen de ne pas vieillir : c’est d’accueillir les années comme elle viennent et avec le sourire… un sourire, c’est toujours jeune. » - Pierre Dac",
+ 
+  "« Pour ce qui est de l’avenir, il ne s’agit pas de le prévoir mais de le rendre possible.» - Antoine de Saint-Exupéry",
+ 
+  "« Si vous ne voulez pas qu’on vous oublie, le jour où vous serez mort et pourri, écrivez des choses qui valent la peine d’être lues ou faites, des choses qui valent la peine d’être écrites. » – Benjamin Franklin",
+ 
+  "« Quand tout semble être contre vous, souvenez-vous que l’avion décolle face au vent, et non avec lui. » – Henry Ford",
+ 
+  "« Ne laissez personne vous voler votre imagination, votre créativité ou votre curiosité. C’est votre place dans le monde, c’est votre vie. » – Mae C. Jemison",
+ 
+  "« Laissez-vous guider par votre rêve, même si vous devez momentanément le mettre de côté pour trouver un emploi ou payer votre loyer. Et restez toujours ouvert aux opportunités de sortir du cadre pour mener la vie et faire les choses qui vous inspirent profondément… n’ayez pas peur. » - Jane Goodall",
+ 
+  "« Quand vous adorez une fleur, vous l’arrachez, mais quand vous aimez une fleur, vous l’arrosez tous les jours. Celui qui comprend ça, comprend la vie. » - Bouddha",
+ 
+  "« J’ai toujours préféré la folie des passions à la sagesse de l’indifférence » - Anatole France",
+ 
+  "« Si vous faites ce que vous avez toujours fait, vous obtiendrez ce que vous avez toujours obtenu. » – Anthony Robbins",
+ 
+  "« Trop d’entre nous ne vivent pas leurs rêves car nous vivons nos peurs. » – Les Brown",
+ 
+  "« La meilleure manière de se lancer, c’est d’arrêter de parler et commencer à agir. » – Walt Disney",
+ 
+  "« On est seul responsable de sa vie. Il ne faut blâmer personne pour ses erreurs. On est soi-même l’artisan de son bonheur et on en est parfois aussi le principal obstacle à son bonheur. Tu es à l’aube de ta vie, je suis au crépuscule de la mienne, je ne peux te donner qu’un conseil: écoute, écoute la petite voix en toi avant de décider quel sera ton chemin. Et le jour où tu entendras cette petite voix, suis la aveuglement… Ne laisse personne décider pour toi. N’aie jamais peur de revendiquer ce qui te tient à coeur. » - Katherine Pancol",
+ 
+  "« J’ai appris que les gens oublieront ce que vous avez dit, ils oublieront ce que vous avez fait, mais ils n’oublieront jamais ce que vous leur avez fait ressentir. » – Maya Angelou",
+ 
+  "« Quand j'étais petit, ma mère m'a dit que le bonheur était la clé de la vie. Quand je suis allé à l'école, ils m'ont demandé ce que je voulais être quand je serais grand. J'ai répondu heureux. Ils m'ont dit que je n'avais pas compris la question. J'ai répondu qu'ils n'avaient pas compris la vie. » - John Lennon",
+ 
+  "« Donnez à chaque jour la chance de devenir le plus beau jour de votre vie. » - Mark Twain",
+ 
+  "« Je veux rester folle, vivre ma vie comme je la rêve et non de la manière imposée par les autres. » - Paulo Coelho",
+ 
+  "« Beaucoup de chemins mènent à la réussite, mais un seul mène immanquablement à l’échec, celui qui consiste à tenter de plaire à tout le monde. » - Benjamin Franklin",
+ 
+  "« Tu ne sais pas à quel point tu es fort jusqu’au jour où être fort devient la seule option. » - Bob Marley",
+ 
+  "« Si votre champ de vision est suffisamment large, vous comprendrez que ce qui vous arrive dans le temps présent vous prépare à un plus grand futur» - Sanaya Roman",
+ 
+  "« Si vous voulez que la vie vous sourie apportez lui d’abord votre bonne humeur » - Spinoza",
+ 
+  "« Si tu veux construire un bateau, ne rassemble pas tes hommes et femmes pour leur donner des ordres, pour expliquer chaque détail, pour leur dire où trouver chaque chose… Si tu veux construire un bateau, fais naître dans le cœur de tes hommes et femmes le désir de la mer. » - Antoine de Saint-Exupéry",
+ 
+  "« 100 % des choses qu’on ne tente pas échouent. » – Wayne Gretzky",
+ 
+  "« Les grandes transformations se font à petits pas. Pose une pierre chaque jour, n’abandonne jamais ta construction, et l’édifice grandira. Combats le doute et la paresse. Tiens constamment ton esprit en éveil. Observe, comprends, et aime. » - Dugpa Rimpoché",
+ 
+  "« Se libérer de la peur, c’est le premier pas, la clef du changement. » - Antonella Verdiani ",
+ 
+  "« Le succès c’est d’aller d’échec en échec sans perdre son enthousiasme. » – Winston Churchill",
+ 
+  "« Hier n’existe plus. Demain ne viendra peut-être jamais. Il n’y a que le miracle du moment présent. Savourez-le. C’est un cadeau » - Marie Stilkind",
+ 
+  "« Souvenez-vous que ne pas obtenir ce que l’on veut peut parfois se révéler être un incroyable coup de chance. » - Tenzin Gyatzo, le 14e Dalaï Lama",
+  "« Le seul homme qui ne se trompe jamais est celui qui ne fait jamais rien. N’ayez pas peur des erreurs, pourvu que vous ne fassiez pas deux fois la même. » - Franklin Delano Roosevelt"
+  ],
+
+  dataImge: ["img/banque-d-images-gratuites-libres-de-droits551-1560x921.jpg",
+  "img/banque-d-images-gratuites-libres-de-droits554-1560x1040.jpg",
+  "img/banque-images-gratuites-libres-de-droits.jpg","imgforet-libre-de-droit-640x320.jpg", 
+  "img/istockphoto-488653428-612x612.jpg", "img/istockphoto-522142798-612x612.jpg", "img/Pixabay-1.jpg"
+   ]
+}
+
+
+const getNumberRandom = num =>{
+  return Math.floor(Math.random() * num);
+}
